1eb095e5aaf1542328e86b22d0208ab7ba359173
Brian Arrieta <brian@spartandevs.com>
2023-09-06 21:21:02 -0400

    [BA] added async/await

---
+++ 02-intro-javascript/src/base/11-async-await.js
@@ -0,0 +1,21 @@
+// const getImagenPromesa = () => new Promise((resolve) => resolve("https://youtube.com"));
+// getImagenPromesa().then(console.log);
+
+const getImagen = async () => {
+  try {
+    const apiKey = "Y1NRgBtsAHl3acFX48UwZzUaPCpbOQm9";
+    const resp = await fetch(
+      `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`
+    );
+    const { data } = await resp.json();
+    const { url } = data.image.original;
+    const img = document.createElement("img");
+    img.src = url;
+    document.body.append(img);
+  } catch (error) {
+    //manejo del error
+    console.error(error);
+  }
+};
+
+getImagen();
