bbb99c6f554b90aa34854e3f6130f426e340acb7
Brian Arrieta <brian@spartandevs.com>
2023-09-09 20:15:10 -0400

    [BA] added crear un nuevo componente

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,10 +1,10 @@
-import React, { StrictMode } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import { HelloWorldApp } from "./HelloWorldApp";
+// import { HelloWorldApp } from "./HelloWorldApp";
+import { FirstApp } from "./FirstApp";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelloWorldApp />
+    <FirstApp/>
   </React.StrictMode>
-);
-
+)
