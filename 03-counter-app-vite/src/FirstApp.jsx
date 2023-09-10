14bb4309460829342596dfc2c83b62290e7766b3
Brian Arrieta <brian@spartandevs.com>
2023-09-09 23:07:20 -0400

    [BA] added Colocar estilos de CSS

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,13 +1,8 @@
-const newMessage = {
-    message: "Hola Mundo",
-    title: "titulo"
-};
-
 export const FirstApp = () => {
   return (
     <>
-    <h1>Esteban</h1>
-    <h1>{JSON.stringify( newMessage )}</h1>
+    <h1>Brian</h1>
+    {/* <code>{JSON.stringify( newMessage )}</code> */}
     <p>Soy un subtitulo</p>
     </>
   )
