b5607c7aa7ad6591785c9bae89bbb45520137e94
Brian Arrieta <brian@spartandevs.com>
2023-09-11 01:33:23 -0400

    [BA] added DefaultProps

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
 
-export const FirstApp = ({title, subTitle}) => {
+export const FirstApp = ({title, subTitle, name}) => {
 
   // console.log(props)
 
@@ -9,11 +9,18 @@ export const FirstApp = ({title, subTitle}) => {
     <h1>{ title }</h1>
     {/* <code>{JSON.stringify( newMessage )}</code> */}
     <p>{subTitle}</p>
+    <p>{name}</p>
     </>
   )
 }
 
 FirstApp.propTypes = {
-  title: PropTypes.string,
-  subTitle: PropTypes.number
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.string
+}
+
+FirstApp.defaultProps = {
+  title: "No hay titulo",
+  subTitle: "No hay subtitulo",
+  name: "Brian Esteban"
 }
