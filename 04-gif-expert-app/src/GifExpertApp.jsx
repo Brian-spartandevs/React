484a8e025665997bde24a359a484d314a158a835
Brian Arrieta <barrieta950@gmail.com>
2023-09-13 18:20:29 -0400

    [BA] added Creando una lista de categorias

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,6 +1,11 @@
-
+import { useState } from "react";
 
 export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch','Dragon Ball'])
+
+console.log(categories)
+
   return (
     <>
     {/* Titulo */}
@@ -10,6 +15,10 @@ export const GifExpertApp = () => {
 
     {/* listado de Gif */}
     {/* Gif Item */}
+    <ol>
+        {categories.map((category)=> <li key={category}>{category}</li> )}
+        {/* <li>ABC</li> */}
+    </ol>
 
 
     </>
