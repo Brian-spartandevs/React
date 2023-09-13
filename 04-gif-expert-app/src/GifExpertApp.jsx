03231c2f84f85f94b9c5c5b90c2537e3e9601bc0
Brian Arrieta <barrieta950@gmail.com>
2023-09-13 16:29:14 -0400

    [BA] added GifExpertApp - Component

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -3,7 +3,15 @@
 export const GifExpertApp = () => {
   return (
     <>
+    {/* Titulo */}
     <h1>GifExpertApp</h1>
+
+    {/* inputs */}
+
+    {/* listado de Gif */}
+    {/* Gif Item */}
+
+
     </>
   )
 }
