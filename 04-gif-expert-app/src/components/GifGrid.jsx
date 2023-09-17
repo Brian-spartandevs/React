53d72130293f5e8c6e4cd98289a2aeef14dd50e5
Brian Arrieta <barrieta950@gmail.com>
2023-09-17 13:38:11 -0400

    [BA] added Fetch API - Obtener las imágenes deseadas

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,7 +1,13 @@
+import { getGifs } from "../helpers/GetGifs";
+
 export const GifGrid = ({category}) => {
+
+  getGifs(category);
+
   return (
     <>
         <h3>{category}</h3>
     </>
   )
 }
+
