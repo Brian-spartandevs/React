3aca9034aa2076ba399d86c378f3c45546fef356
Brian Arrieta <barrieta950@gmail.com>
2023-10-10 19:46:23 -0400

    [BA] added useFetch + useCounter

--- 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
+++ 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
@@ -2,14 +2,35 @@ import { useFetch } from "../hooks/useFetch"
 
 export const MultipleCustomHooks = () => {
 
-  const { data, isLoading, hasError } = useFetch('https://rickandmortyapi.com/api/character/2')
-console.log({ data, isLoading, hasError })
+  const { data, isLoading, hasError } = useFetch('https://api.breakingbadquotes.xyz/v1/quotes/')
+
+  const {author, quote} = !!data && data[0]
 
   return (
     <>
-        <h1>RickAndMorty: Characters</h1>
+        <h1>BreakingBad Quotes</h1>
         <hr />
-    
+
+        {
+          isLoading
+              ? (
+                    <div className="alert alert-info text-center">
+                        Loading...
+                    </div>
+              )
+              : (
+                    <blockquote className="blockquote text-end">
+                        <p className="mb-1">{quote}</p>
+                        <footer className="blockquote-footer">{author}</footer>
+                    </blockquote>
+              )
+        }
+
+
+        <button className="btn btn-primary">
+          Next Quote
+        </button>
+
     </>
   )
 }
