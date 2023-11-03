8eac5848065a1881fa2da7976dc9db97130192e5
Brian Arrieta <barrieta950@gmail.com>
2023-11-02 21:45:07 -0400

    [BA] added Continuación del Proyecto - HookApp

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -15,10 +15,11 @@ import './index.css'
 // import { CallbackHook } from './06-memos/CallbackHook'
 import { Padre } from './07-tarea-memo/Padre'
 
+import './08-useReducer/intro-reducer'
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <Padre/>
-  // </React.StrictMode>
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   // <React.StrictMode>
+//     <Padre/>
+//   // </React.StrictMode>
+// )
