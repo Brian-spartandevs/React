20991cf29f1aa5ef823bfe9b1e64346ff8b83fe7
Brian Arrieta <barrieta950@gmail.com>
2023-11-09 19:06:15 -0400

    [BA] added Resolución de la tarea - TodoApp

--- 05-hook-app/src/08-useReducer/TodoApp.jsx
+++ 05-hook-app/src/08-useReducer/TodoApp.jsx
@@ -1,5 +1,7 @@
 import { useReducer } from "react"
 import { todoReducer } from "./todoReducer"
+import { TodoList } from "./TodoList"
+import { TodoAdd } from "./TodoAdd"
 
 const initialState = [
     {
@@ -30,42 +32,13 @@ export const TodoApp = () => {
 
             <div className="row">
                 <div className="col-7">
-
-                    {/* TodoList */}
-                    <ul className="list-group">
-                        {
-                            todos.map( todo => (
-                                // TodoItem ....
-                                <li key={ todo.id } className="list-group-item d-flex justify-content-between">
-                                    <span className="align-self-center">Item 1</span>
-                                    <button className="btn btn-danger">Borrar</button>
-                                </li>
-                            ))
-                        }
-                    </ul>
-                    {/* Fin TodoList */}
+                    <TodoList todos={ todos } />
                 </div>
 
                 <div className="col-5">
                     <h4>Agregar TODO</h4>
                     <hr />
-                    {/* TodoAdd onNewTodo( todo ) */}
-                    {/* {id: new Date()..., description: '', done: false } */}
-                    <form>
-                        <input 
-                            type="text"
-                            placeholder="¿Qué hay que hacer?"
-                            className="form-control"
-                        />
-
-                        <button 
-                            type="submit"
-                            className="btn btn-outline-primary mt-1"
-                        >
-                            Agregar
-                        </button>
-                    </form>
-                    {/* Fin TodoAdd */}
+                    <TodoAdd onNewTodo={ handleNewTodo } />
                 </div>
 
             </div>
