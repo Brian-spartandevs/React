cfd1452ca9da1528e8e02fdc0fbbf17aae2953d9
Brian Arrieta <barrieta950@gmail.com>
2023-11-13 19:19:38 -0400

    [BA] added Link

--- 05-hook-app/src/09-useContext/MainApp.jsx
+++ 05-hook-app/src/09-useContext/MainApp.jsx
@@ -1,13 +1,15 @@
-import { Navigate, Route, Routes } from "react-router-dom"
+import { Link, Navigate, Route, Routes } from "react-router-dom"
 import { HomePage } from "./HomePage"
 import { LoginPage } from "./LoginPage"
 import { AboutPage } from "./AboutPage"
+import { NavBar } from "./NavBar"
 
 
 export const MainApp = () => {
   return (
     <>
         <h1>MainApp</h1>
+          <NavBar />
         <hr />
 
         <Routes>
