ea58ec926a50640edb50b6abb815194b4185d32f
Brian Arrieta <barrieta950@gmail.com>
2023-11-22 01:43:34 -0400

    [BA] added Tarjeta del Héroe - parte 2

--- 07-heroes-spa/src/heroes/components/HeroCard.jsx
+++ 07-heroes-spa/src/heroes/components/HeroCard.jsx
@@ -1,8 +1,21 @@
+import { Link } from 'react-router-dom'
+
+const CharactersByHero = ({ alter_ego, characters }) => {
+    // if ( alter_ego === characters ) return (<></>);
+    // return <p>{ characters }</p>
+    return ( alter_ego === characters )
+    ? <></>
+    : <p>{ characters }</p>
+
+}
+
 
 
 export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance, characters }) => {
 
     const heroImageUrl = `/assets/heroes/${ id }.jpg`
+
+    // const charactersByHero = ( <p>{ characters }</p> )
 
   return (
     <div className="col" >
@@ -21,7 +34,21 @@ export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance
                         <h5 className="card-title">{ superhero }</h5>
                         <p className="card-text">{ alter_ego }</p>
 
-                        <p>{ characters }</p>
+                        {/* {
+                            ( alter_ego !== characters ) && charactersByHero
+                            ( alter_ego !== characters ) && ( <p>{ characters }</p> )
+                        } */}
+
+                        <CharactersByHero alter_ego={ alter_ego } characters={ characters } />
+
+                        <p className="card-text">
+                            <small className="text-muted">{ first_appearance }</small>
+                        </p>
+
+
+                        <Link to={`/hero/${ id }`} >
+                            Más..
+                        </Link>
 
                     </div>
 
