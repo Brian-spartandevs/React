e1e14d190016918993984c7d2d008acb15532bfc
Brian Arrieta <barrieta950@gmail.com>
2024-01-02 00:55:44 -0400

    [BA] added Mostrar mensajes condicionales

--- 07-heroes-spa/src/heroes/pages/SearchPage.jsx
+++ 07-heroes-spa/src/heroes/pages/SearchPage.jsx
@@ -11,8 +11,10 @@ export const SearchPage = () => {
   const location = useLocation();
 
   const { q = '' } = queryString.parse( location.search );
-
   const heroes = getHeroesByName( q );
+
+  const showSearch = (q.length === 0);
+  const showError = (q.length > 0) && (heroes.length === 0);
 
   const { searchText, onInputChange } = useForm({
     searchText: q
@@ -20,7 +22,7 @@ export const SearchPage = () => {
 
   const onSearchSubmit = ( event ) => {
     event.preventDefault()
-    if ( searchText.trim().length <= 1 ) return;
+    // if ( searchText.trim().length <= 1 ) return;
 
     navigate(`?q=${ searchText }`)
   }
@@ -56,11 +58,20 @@ export const SearchPage = () => {
           <h4>Results</h4>
           <hr />
 
-          <div className="alert alert-primary">
+          {/* {
+            ( q === '' )
+            ? <div className="alert alert-primary">Search a hero</div>
+            : ( heroes.length === 0 ) 
+              && <div className="alert alert-danger">No hero found with <b>{ q }</b></div>
+          } */}
+
+          <div className="alert alert-primary animate__animated animate__fadeIn" 
+              style={{ display: showSearch ? '' : 'none' }}>
             Search a hero
           </div>
 
-          <div className="alert alert-danger">
+          <div className="alert alert-danger animate__animated animate__fadeIn" 
+              style={{ display: showError ? '' : 'none' }}>
             No hero found with <b>{ q }</b>
           </div>
 
